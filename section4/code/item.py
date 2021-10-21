722ef867101d6b1f0c49cafeaa6b9eb4ded4098d
Esuabom Dijemeni <e.dijemeni@gmail.com>
2021-10-21 17:56:37 +0100

    Delete and update method for items

--- section4/code/item.py
+++ section4/code/item.py
@@ -39,6 +39,15 @@ class Item(Resource):
         data = Item.parser.parse_args()
         item = {'name':name, 'price':data['price']}
 
+        try:
+            self.insert(item)
+        except:
+            return {"message":"An error occured inserting the item."}, 500 # Server error
+
+        return item, 201
+
+    @classmethod
+    def insert(cls, item):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
@@ -47,24 +56,48 @@ class Item(Resource):
 
         connection.commit()
         connection.close()
-        
-        return item, 201
 
     def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "DELETE FROM  items WHERE name=?"
+        cursor.execute(query, (name,))
+
+        connection.commit()
+        connection.close()
+
         return {'message':'Item deleted'}
 
     def put(self, name):
         data = Item.parser.parse_args()
 
-        item = next(filter(lambda x:x['name'] == name, items), None)
+        item = self.find_by_name(name)
+        updated_item = {'name':name, 'price':data['price']}
+
+
         if item is None:
-             item = {'name':name, 'price':data['price']}
-             items.append(item)
+            try:
+                self.insert(updated_item)
+            except:
+                return {"message": "An error occured inserting the item"}, 500
         else:
-            item.update(data)
-        return item
+            # try:
+            self.update(updated_item)
+            # except:
+            #     return {"message": "An error occured updating the item"}, 500
+        return updated_item
+
+    @classmethod
+    def update(cls, item):
+        connection = sqlite3.connect("data.db")
+        cursor = connection.cursor()
+
+        query = "UPDATE items SET price=? WHERE name=?"
+        cursor.execute(query, (item['price'], item['name']))
+
+        connection.commit()
+        connection.close()
 
 class ItemList(Resource):
 
